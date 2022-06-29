2bb081e4f95404bf1e0f5017a147592431815f42
ju8ko <julia.x.kobzar@gmail.com>
2022-06-29 16:20:38 +0200

    Add 1-8 tasks

---
+++ solution.js
@@ -0,0 +1,30 @@
+//0.
+const apples = 20;
+const oranges = 30;
+
+//1.
+console.log(apples === oranges);
+
+//2.
+console.log(apples !== oranges);
+
+//3.
+console.log(apples > oranges);
+
+//4.
+console.log(apples <= oranges);
+
+//5.
+console.log(oranges > apples);
+
+//6.
+const mangoes = 5;
+console.log(mangoes * apples > oranges + mangoes);
+
+//7.
+console.log(apples - mangoes < oranges / mangoes);
+
+//8.
+console.log(mangoes === apples && apples === oranges);
+
+//9.
